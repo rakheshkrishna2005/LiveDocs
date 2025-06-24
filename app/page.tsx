2f47fc626e6ff2583bfd7a6a7488479c54e8cdc9
Rakhesh Krishna P <rakheshkrishnap@gmail.com>
2025-06-24 09:40:41 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="animate-pulse">Redirecting...</div>
+      <div className="animate-pulse">Loading...</div>
     </div>
   )
 }
